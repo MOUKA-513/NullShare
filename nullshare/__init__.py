79fe0e1bc5ab8b92007531a0806bc3a54e557cf0
MOUKA-513 <adnanemoussaoui.513@gmail.com>
2026-01-10 10:36:39 -0500

    __init__.py

--- nullshare/__init__.py
+++ nullshare/__init__.py
@@ -3,5 +3,5 @@ NullShare - Share files via QR code on local network
 """
 
 __version__ = "0.1.0"
-__author__ = "MOOKA-513"
+__author__ = "MOUKA-513"
 __description__ = "No internet, no cloud, just local WiFi file sharing"
